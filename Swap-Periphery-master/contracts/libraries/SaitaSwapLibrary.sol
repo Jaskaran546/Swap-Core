0322b94d0c02dcfbcd8c0d8827c5c535e55d841b
Jaskaran Singh <jaskarn.singh@antiersolutions.com>
2022-06-21 19:16:18 +0530

    Audit Updates

--- Swap-Periphery-master/contracts/libraries/SaitaSwapLibrary.sol
+++ Swap-Periphery-master/contracts/libraries/SaitaSwapLibrary.sol
@@ -28,7 +28,6 @@ library SaitaSwapLibrary {
     // fetches and sorts the reserves for a pair
     function getReserves(address factory, address tokenA, address tokenB) internal view returns (uint reserveA, uint reserveB) {
         (address token0,) = sortTokens(tokenA, tokenB);
-        pairFor(factory, tokenA, tokenB);
         (uint reserve0, uint reserve1,) = ISaitaSwapPair(pairFor(factory, tokenA, tokenB)).getReserves();
         (reserveA, reserveB) = tokenA == token0 ? (reserve0, reserve1) : (reserve1, reserve0);
     }
